42840204a69595384a61acb0aee3b3077112d8ee
Wout Cherlet <Wout.Cherlet@Ugent.be>
2023-05-08 15:24:59 +0200

    labeled voxels working + plot colors

--- lidar2mc/voxelization.py
+++ lidar2mc/voxelization.py
@@ -1,4 +1,4 @@
-from typing import Union
+from collections import Counter
 
 import numpy as np
 import open3d as o3d
@@ -15,12 +15,14 @@ class VoxelNode:
     def add_point(self, point, label=None):
         self.points.append(point)
         self.n_points += 1
-        if label:
+        if label is not None:
             self.labels.append(label)
 
-    def get_voxel_label(self, labelname):
-        pass
-        # check if labelname exists for points, then return most common one/ average?
+    def get_voxel_label(self):
+        # return most common label, if draw it returns a random one
+        if len(self.labels) > 0:
+            return Counter(self.labels).most_common(1)[0][0]
+        return None
 
 
 
@@ -29,6 +31,12 @@ class VoxelGrid:
         self.min_points = min_points_per_voxel
 
         pts_np = pointcloud.point.positions.numpy()
+        try:
+            labels = pointcloud.point.labels.numpy()
+            self.labels_present = True
+        except:
+            print("No labels found")
+            self.labels_present = False
 
         # get max and min bounds of pointcloud, calculate amount of bins in each dimension and init 3D voxel array
 
@@ -38,47 +46,82 @@ class VoxelGrid:
         self.n_bins = ranges//resolution + 1
         
         # init voxelgrid array
-
         self.voxel_array = np.array([[[None for _ in range(int(self.n_bins[2]))] for _ in range(int(self.n_bins[1]))] for _ in range(int(self.n_bins[0]))])
 
-        # TODO: parallelize with numpy ? smarter way to do this: first do count, some map reduce type structure?
-
-        for point in pts_np:
+        for i, point in enumerate(pts_np):
             idxs = (point - min_bounds) // resolution
             idxs = tuple(idxs.astype(int))
 
             if self.voxel_array[idxs] is None:
                 self.voxel_array[idxs] = VoxelNode(idxs)
-            self.voxel_array[idxs].add_point(point)
+            if self.labels_present:
+                self.voxel_array[idxs].add_point(point, label=labels[i])
+            else:
+                self.voxel_array[idxs].add_point(point)
 
         for index in np.ndindex(self.voxel_array.shape):
             if self.voxel_array[index] and self.voxel_array[index].n_points < self.min_points:
                 self.voxel_array[index] = None
 
     def get_voxels(self):
-        # create list of only occupied voxel
-        pass
+        return self.voxel_array[self.voxel_array != np.array(None)]
 
     def plot(self):
         # plot using matplotlib
 
+        # construct color array
+        if self.labels_present:
+            colors = np.empty(self.voxel_array.shape, dtype=object)
+            colormap = {1: 'brown', 0: 'green'}
+            for index in np.ndindex(self.voxel_array.shape):
+                if self.voxel_array[index]:
+                    label = self.voxel_array[index].get_voxel_label()
+                    if label is not None:
+                        colors[index] = colormap[label]
+        else:
+            colors = "green"
+
         boolean_voxels = self.voxel_array != None
         
         ax = plt.figure().add_subplot(projection='3d')
-        ax.voxels(boolean_voxels, edgecolor='k')
+        ax.voxels(boolean_voxels, facecolors=colors, edgecolor='k')
         ax.set_aspect('equal')
 
         plt.show()
 
+def read_txt_file(path):
+    points = []
+    labels = []
+    with open(path, 'r') as f:
+        for line in f.readlines():
+            els = line.split(' ')
+            points.append([float(els[0]), float(els[1]), float(els[2])])
+            labels.append(int(float(els[3].replace("\n", ""))))
+    
+    device = o3d.core.Device("CPU:0")
 
-def test_voxels(path):
-    pc = o3d.t.io.read_point_cloud(path)
+    pcd = o3d.t.geometry.PointCloud(device)
 
-    vxlgrid = VoxelGrid(pc, resolution=0.5, min_points_per_voxel=100)
+    pcd.point.positions = o3d.core.Tensor(points, o3d.core.float32, device)
+
+    pcd.point.labels = o3d.core.Tensor(labels, o3d.core.int32, device)
+
+    return pcd
+
+
+def test_voxels(pc):
+    vxlgrid = VoxelGrid(pc, resolution=0.5, min_points_per_voxel=50)
 
     vxlgrid.plot()
 
-
 if __name__ == "__main__":
-    test_path = r"C:\Users\wcherlet\OneDrive - UGent\data\singletrees\wytham_winter_5a.ply"
-    test_voxels(test_path)
+    # text file with labels
+    test_path = r"C:\Users\wcherlet\OneDrive - UGent\data\singletrees\separated\dro_033_pc.txt"
+    pc = read_txt_file(test_path)
+
+    # ply file
+    # test_path = r"C:\Users\wcherlet\OneDrive - UGent\data\singletrees\wytham_winter_5a.ply"
+    # pc = o3d.t.io.read_point_cloud(test_path)
+
+
+    test_voxels(pc)
